835921a6c88c14711f8e2cb3744f4639ede2e109
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2020-12-10 16:16:09 +0200

    Update license to Apache v2

--- anchor-nav-demo/src/main/java/com/vaadin/componentfactory/demo/AnchorNavView.java
+++ anchor-nav-demo/src/main/java/com/vaadin/componentfactory/demo/AnchorNavView.java
@@ -1,22 +1,5 @@
 package com.vaadin.componentfactory.demo;
 
-/*
- * #%L
- * Vaadin Component Factory Anchor Nav Example for Vaadin 14
- * %%
- * Copyright (C) 2017 - 2018 Vaadin Ltd
- * %%
- * This program is available under Commercial Vaadin Add-On License 3.0
- * (CVALv3).
- *
- * See the file license.html distributed with this software for more
- * information about licensing.
- *
- * You should have received a copy of the CVALv3 along with this program.
- * If not, see <http://vaadin.com/license/cval-3>.
- * #L%
- */
-
 import com.vaadin.componentfactory.AnchorNav;
 import com.vaadin.componentfactory.AnchorNavSection;
 import com.vaadin.componentfactory.AnchorNavVariant;
--- anchor-nav/src/main/java/com/vaadin/componentfactory/AnchorNav.java
+++ anchor-nav/src/main/java/com/vaadin/componentfactory/AnchorNav.java
@@ -1,5 +1,25 @@
 package com.vaadin.componentfactory;
 
+/*
+ * #%L
+ * Anchor Nav for Flow
+ * %%
+ * Copyright (C) 2020 Vaadin Ltd
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,7 +38,7 @@ import com.vaadin.flow.component.html.H2;
  * @author Vaadin Ltd
  */
 @Tag("vcf-anchor-nav")
-@NpmPackage(value = "@vaadin-component-factory/vcf-anchor-nav", version = "1.0.16")
+@NpmPackage(value = "@vaadin-component-factory/vcf-anchor-nav", version = "1.1.3")
 @JsModule("@vaadin-component-factory/vcf-anchor-nav")
 @SuppressWarnings("serial")
 public class AnchorNav extends HtmlContainer implements HasTheme {
