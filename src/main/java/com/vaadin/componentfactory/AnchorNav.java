f6681bc6e51cd4ea7236bac7e89842d69f71052f
Paola De Bartolo <paola.debartolo@gmail.com>
2026-07-24 14:42:00 -0300

    build: update web-component version to 3.0.2
    This version adds more specificity in selection queries.

--- src/main/java/com/vaadin/componentfactory/AnchorNav.java
+++ src/main/java/com/vaadin/componentfactory/AnchorNav.java
@@ -43,7 +43,7 @@ import java.util.stream.Stream;
  * @author Vaadin Ltd
  */
 @Tag("vcf-anchor-nav")
-@NpmPackage(value = "@vaadin-component-factory/vcf-anchor-nav", version = "3.0.1")
+@NpmPackage(value = "@vaadin-component-factory/vcf-anchor-nav", version = "3.0.2")
 @JsModule("@vaadin-component-factory/vcf-anchor-nav")
 @SuppressWarnings("serial")
 public class AnchorNav extends HtmlContainer implements HasTheme {
